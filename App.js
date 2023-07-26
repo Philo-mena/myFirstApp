51fea3b038d56852d8e85bb48d3a0b95e69d8ca8
Philomena <philomenaobeng27@gmail.com>
2023-07-26 23:31:22 +0000

    first comment

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export default function App() {
+import picture from "./assets/picture.jpg";
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={picture} style={styles.image} />
+      <View style={styles.secondContainer}>
+        <Text style={styles.label}>Name</Text>
+        <Text style={[styles.info, styles.label]}>Cassanda Williams</Text>
+      </View>
+
+      <View style={styles.secondContainer}>
+        <Text style={styles.label}>Email</Text>
+        <Text style={[styles.info, styles.label]}>cassy@gmail.com</Text>
+      </View>
+
+      <View style={styles.secondContainer}>
+        <Text style={styles.label}>Gender</Text>
+        <Text style={[styles.info, styles.label]}>Female</Text>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +29,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 5,
+  },
+
+  image: {
+    marginTop: 100,
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+    alignSelf: "center",
+    borderRadius: 75,
+  },
+
+  secondContainer: {
+    flexDirection: "row",
+    marginVertical: 3,
+  },
+
+  label: {
+    borderColor: "black",
+    borderWidth: "1",
+    fontSize: 20,
+    flex: 2.5,
+    paddingHorizontal: 5,
+  },
+
+  info: {
+    flex: 7.5,
   },
 });
+
+export default App;
